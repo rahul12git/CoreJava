5c349a7b88c55cf1ce6144ab8d4a9d8549b35109
rahul <156104194+rahul12git@users.noreply.github.com>
2024-01-11 20:05:09 +0530

    Testing Git

--- CoreJava/src/PracticeDSA/SortingAlogorithm/M2.java
+++ CoreJava/src/PracticeDSA/SortingAlogorithm/M2.java
@@ -1,7 +1,7 @@
 package PracticeDSA.SortingAlogorithm;
 
-public class Main {
+public class M2 {
     public static void main(String[] args) {
-        System.out.println(200);
+        System.out.println("M");
     }
 }
